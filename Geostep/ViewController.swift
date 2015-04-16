1a6c18ad944d558f044dc7ccbd1185613e086194
misur <winddoo@Winds-Mac-mini.local>
2015-04-16 14:12:24 +0200

    Initial Commit

---
+++ Geostep/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Geostep
+//
+//  Created by Wind on 16/04/15.
+//  Copyright (c) 2015 Wind. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
